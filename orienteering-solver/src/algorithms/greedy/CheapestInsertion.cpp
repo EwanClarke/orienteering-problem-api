ae093e0b95b48548a21bf2bbd1f9d4e8a263fa3f
Ewan <ewan.o.clarke@gmail.com>
2025-10-02 12:01:00 +0100

    Insertion bindings

--- orienteering-solver/src/algorithms/greedy/CheapestInsertion.cpp
+++
@@ -1,7 +0,0 @@
-#include "CheapestInsertion.h"
-#include "../../OrienteeringProblemInputData.h"
-#include <vector>
-
-std::vector<int> CheapestInsertion(const OrienteeringProblemInputData& input){
-    return std::vector<int>{input.startNode, input.endNode};
-}
