912e849da9806454b189a33f005b0af0621bc45e
Ewan <ewan.o.clarke@gmail.com>
2025-10-24 17:40:26 +0100

    updated build system to install all required modules and libraries

--- orienteering_prep/matrix_construction.py
+++ orienteering_prep/matrix_construction.py
@@ -1,4 +1,8 @@
 from geopy.distance import geodesic
+import requests
+import os
+from dotenv import load_dotenv
+
 
 def construct_geodesic_adjacency_matrix(locations):
     n = len(locations)
@@ -17,3 +21,20 @@ def construct_geodesic_adjacency_matrix(locations):
     return adjacency_matrix
 
 
+def construct_osr_adjacency_matrix(locations):
+    body = {"locations": [[loc.lon, loc.lat] for loc in locations],
+            "metrics": ["duration"]}
+
+    load_dotenv()
+    headers = {
+        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+        'Authorization': os.getenv('ORS_API_KEY'),
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+    call = requests.post('https://api.openrouteservice.org/v2/matrix/driving-car', json=body, headers=headers)
+
+    if call.status_code != 200:
+        raise Exception(f"Error fetching OSR matrix: {call.status_code} {call.reason} {call.text}")
+
+    data = call.json()
+    return data['durations']
