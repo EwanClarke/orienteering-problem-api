2bb8456350cf95d846fa6a5d1c827eab45496e82
Ewan <ewan.o.clarke@gmail.com>
2025-10-24 16:17:57 +0100

    removed unused pyproject.toml and setup.py files in root directory

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-from setuptools import setup, find_packages
-
-setup(
-    name='orienteering_project',
-    version='0.1.0',
-    packages= ["orienteering_solver", "orienteering_prep"],
-    # Other metadata like install_requires for libraries like fastapi, etc.
-)
